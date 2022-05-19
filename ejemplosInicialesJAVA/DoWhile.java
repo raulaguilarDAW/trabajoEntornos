7cf3e1db072bb061e0e0ae4dedbef29d751574fd
raulaguilarDAW <aguilar_garcia@pop-os.localdomain>
2022-05-19 12:26:26 +0200

    Añadir descripción de la clase DoWhile

--- ejemplosInicialesJAVA/DoWhile.java
+++ ejemplosInicialesJAVA/DoWhile.java
@@ -3,17 +3,20 @@ package ejemplosInicialesJAVA;
 public class DoWhile {
 
 	public static void main(String[] args) {
+
+		// DescripciÃ³n del ejercicio
+		System.out.println("Contador de numeros hasta el 10");
+
 		int num=1;
 		System.out.println("Contando...");
-		
-		
+
+
 		do{
 			System.out.println("Vamos por el número "+num);
 			num++;
 		}while(num<=10);
-		
+
 		System.out.println("Fin de la cuenta");
 	}
-	
 
 }
