05d5ec3a4e46238b11876ffd9a776be940a408cd
raulaguilarDAW <ragugar0404@g.educaand.es>
2022-05-22 11:51:34 +0200

    Commit a miNuevaRamita

--- ejemplosInicialesJAVA/EjemploCasting.java
+++ ejemplosInicialesJAVA/EjemploCasting.java
@@ -3,17 +3,20 @@ package ejemplosInicialesJAVA;
 public class EjemploCasting {
 
 	public static void main(String[] args) {
-			
+
+		// Para probar miNuevaRamita ######################################################
+		System.out.println("DivisiÃ³n entre 2 nÃºmeros");
+
 		int num1;
 		int num2;
 		double division;
-		
+
 		System.out.println("Introduzca un número:");
 		num1=Entrada.entero();
-		
+
 		System.out.println("Introduzca otro:");
 		num2=Entrada.entero();
-		
+
 		 division=((double)num1/(double)num2); //12 entre 7 devuelve 1 
 		//division=(double)(num1/num2); //12 entre 7 devuelve.....
 		System.out.println("La división entera entre "+num1+" y "+num2+" es "+division);
